816f69c2c8579788e67562909354ed3c13617f6d
junqi21263 <junqi21263@gmail.com>
2026-09-04 17:13:53 +0800

    feat: scaffold capacitor android app

---
+++ android/app/src/main/java/com/lewislee/nordicnutri/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lewislee.nordicnutri;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lewislee.nordicnutri',
+  appName: 'Nordic Nutri',
+  webDir: 'mini-program/dist/h5',
+};
+
+export default config;
